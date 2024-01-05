73ca8a7b81490cb9127ee157eb677c3676dc05b0
musafarahamed <musafar.innerix@gmail.com>
2024-01-05 15:26:56 +0530

    auth changes

---
+++ lib/common/common_widgets.dart
@@ -0,0 +1,31 @@
+
+import 'package:flutter/material.dart';
+
+import '../constants/colors.dart';
+class CustomTextFormField extends StatelessWidget {
+  final IconData icon;
+  final bool obscureText;
+  final String hintText;
+  final TextEditingController? controller;
+
+  const CustomTextFormField({
+    Key? key, required this.icon, required this.obscureText, required this.hintText,this.controller,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      obscureText: obscureText,
+      style: TextStyle(
+        color: AppColors.blackColor,
+      ),
+      decoration: InputDecoration(
+        border: InputBorder.none,
+        prefixIcon: Icon(icon, color: AppColors.blackColor.withOpacity(.3),),
+        hintText: hintText,
+      ),
+      cursorColor: AppColors.blackColor.withOpacity(.5),
+    );
+  }
+}
